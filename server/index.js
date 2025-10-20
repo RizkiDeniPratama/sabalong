3822cff0e4788909d48266a6a6f45ead9ad329b8
Deni <denismw172@gmail.com>
2025-10-20 11:12:57 +0800

    memperbaiki struktur folder dan koneksi database postgresql dengan prisma

--- server/index.js
+++ server/index.js
@@ -1,24 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 
+const prisma = new PrismaClient();
+
 // Create an Express application
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
-// midlleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// FUNGSI UNTUK MENGETES KONEKSI DATABASE
+async function testDbConnection() {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+    console.log("✅ Database connection successful!");
+  } catch (error) {
+    console.error("❌ Failed to connect to the database.");
+    console.error(error);
+    process.exit(1);
+  }
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+testDbConnection();
+
+// Main route
+app.get("/", (req, res) => {
+  res.send("Hello, World from the API!");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-module.exports = app
+// export default app;
